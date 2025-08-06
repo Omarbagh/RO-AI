78b23713e2780faebc8bfeff69b72a1e3d5993ae
Omarbagh <o.elbaghouri@icloud.com>
2025-08-06 22:52:27 +0200

    refactor: remove unused showPrintNotification prop from FinalPageStep component

--- src/app/editor/components/steps/FinalPageStep.tsx
+++ src/app/editor/components/steps/FinalPageStep.tsx
@@ -6,7 +6,6 @@ import { useState, useEffect } from "react"
 
 interface FinalPageStepProps {
   handlePrint: () => void;
-  showPrintNotification: boolean;
 }
 
 export default function FinalPageStep({ handlePrint }: FinalPageStepProps) {
